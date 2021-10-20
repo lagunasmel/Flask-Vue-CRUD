ae8720814ae29bb75d4fdb0533118bc9c6801335
Melissa Lagunas <lagunasmel@gmail.com>
2021-10-20 12:19:02 -0400

    Modify Books

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container text-center mx-auto">
     <div class="row">
       <div class="col-sm-10">
         <h1>Books</h1>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Books from '../components/Books.vue';
 import Ping from '../components/Ping.vue';
+import Home from '../views/Home.vue'
 
 Vue.use(Router);
 
@@ -9,7 +10,7 @@ export default new Router({
     mode: 'history',
     base: process.env.BASE_URL,
     routes: [{
-            path: '/',
+            path: '/books',
             name: 'Books',
             component: Books,
         },
@@ -18,5 +19,10 @@ export default new Router({
             name: 'Ping',
             component: Ping,
         },
+        {
+            path: "/",
+            "name": 'Home',
+            component: Home,
+        }
     ],
 });
